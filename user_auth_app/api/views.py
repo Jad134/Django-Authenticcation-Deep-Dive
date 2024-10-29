8d7f312c382caa1295b86ed2612b222451b92c3a
unknown <jadelnader99@gmail.com>
2024-10-29 17:20:58 +0100

    add custom login

--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import  AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 class UserProfileList(generics.ListCreateAPIView):
@@ -30,6 +31,27 @@ class RegistrationView(APIView):
               'token': token.key,
               'username':saved_account.username,
               'email': saved_account.email
+          }
+      else:
+          data=serializer.errors
+
+      return Response(data)
+        
+
+class CustomLoginView(ObtainAuthToken):
+  permission_classes = [AllowAny]
+
+  def post(self, request):
+      serializer = self.serializer_class(data= request.data)
+
+      data = {}
+      if serializer.is_valid():
+          user = serializer.validated_data['user']
+          token, created = Token.objects.get_or_create(user = user)
+          data = {
+              'token': token.key,
+              'username':user.username,
+              'email': user.email
           }
       else:
           data=serializer.errors
